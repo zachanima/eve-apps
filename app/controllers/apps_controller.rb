cfc06787d348faae16fce04ea86600e38f288de6
Zach Ewert <zachanima@gmail.com>
2011-05-06 00:35:13 +0200

    Add initial harvesting apps framework

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -6,24 +6,42 @@ class AppsController < ApplicationController
   end
 
   def show
-    @types = [34,35,36,37,38,39,40]
-    @inputs = {
-      22 => 'Arkonor',
-      1223 => 'Bistot',
-      1225 => 'Crokite',
-      1232 => 'Dark Ochre',
-      1229 => 'Gneiss',
-      21 => 'Hedbergite',
-      1231 => 'Hemorphite',
-      1226 => 'Jaspet',
-      20 => 'Kernite',
-      1227 => 'Omber',
-      18 => 'Plagioclase',
-      1224 => 'Pyroxeres',
-      1228 => 'Scordite',
-      19 => 'Spodumain',
-      1230 => 'Veldspar'
-    }
+    case @app.id
+      when 1
+        @types = [34,35,36,37,38,39,40]
+        @inputs = {
+          22 => 'Arkonor',
+          1223 => 'Bistot',
+          1225 => 'Crokite',
+          1232 => 'Dark Ochre',
+          1229 => 'Gneiss',
+          21 => 'Hedbergite',
+          1231 => 'Hemorphite',
+          1226 => 'Jaspet',
+          20 => 'Kernite',
+          1227 => 'Omber',
+          18 => 'Plagioclase',
+          1224 => 'Pyroxeres',
+          1228 => 'Scordite',
+          19 => 'Spodumain',
+          1230 => 'Veldspar'
+        }
+
+      when 2
+        @types = [30375,30376,30377,30370,30378,30371,30372,30373,30374]
+        @inputs = {
+          30375 => 'Fullerite-C28',
+          30376 => 'Fullerite-C32',
+          30377 => 'Fullerite-C320',
+          30370 => 'Fullerite-C50',
+          30378 => 'Fullerite-C540',
+          30371 => 'Fullerite-C60',
+          30372 => 'Fullerite-C70',
+          30373 => 'Fullerite-C72',
+          30374 => 'Fullerite-C84'
+        }
+      else @inputs = { }
+    end
     update_values(@types)
   end
 
--- app/helpers/apps_helper.rb
+++ app/helpers/apps_helper.rb
@@ -1,8 +1,14 @@
 module AppsHelper
   def generate_json(types)
+    types.each do |type|
+      unless InvType.exists? type
+        InvType.create!(:type_id => type)
+      end
+    end
+
     InvType.find_all_by_type_id(types).collect do |type|
   "#{type.type_id}: {
-    volume: #{type.volume},
+    volume: #{type.volume || 0},
     buy: #{type.buy || 0},
     sell: #{type.sell || 0}
   },
